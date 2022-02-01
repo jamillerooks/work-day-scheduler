4f534931ba1b6eb69ed715d0b72738e600d2b1cc
Jamille Rooks <jamillerooks@outlook.com>
2022-02-01 00:29:53 -0500

    The color code change to reflect past, present and future on the schedule

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
-var currentDayEl = $('#currentDay');
-const m = moment(); 
+$(document).ready(() => {
 
+});
+  //Enter today's date in the header
+  var currentDayEl = $('#currentDay');
+    const m = moment(); 
 
-// handle displaying the time
-function displayDate() {
+  function displayDate() {
     var presentDay = moment().format('dddd, MMMM Do');
     currentDayEl.text(presentDay);
+}
+setInterval(displayDate, 1000);
+
+//Elements from DOM
+  var currentTime = moment();
+  var currentHour = parseInt(moment().format("HH"));
+  var formEl = $('.calendar-form');
+  var hourEl = $('.hour');
+  var descriptionEl = $('.description');
+  var saveBtn = $('.saveBtn');
+
+//Change color based on the time of the appointment
+descriptionEl.each(function(index){
+  var rowTime = $(this).attr("id")
+  if(parseInt(rowTime) < currentHour) {
+    $(this).attr("class", "description col-8 past")
+  }else if(parseInt(rowTime) == currentHour) {
+    $(this).attr("class", "description col-8 present")
+  }else {
+    $(this).attr("class", "description col-8 future")
   }
+})
+
   
-  
-  setInterval(displayDate, 1000);
+//Save appointment in the local storage
+$('.saveBtn').on('click', saveItem);
 
  
+  function saveItem (event) {
+    event.preventDefault();
+    var description = ($(this).prev().val());
+    var hour =   ($(this).prev().prev().text());
+    if (description !== "")
+    localStorage.setItem(hour, description);
+     
+    };
+
+    function renderLastRegistered() {
+      var hour = localStorage.getItem("hour");
+      var description = localStorage.getItem("description");
+    
+      if (hour && description) {
+        return;
+      }
+        hourEl.textContent = hour;
+        descriptionEl.textContent = description;
+    }
